cf427aae6fcdae65f8cbaccaf73a6f66a12c0bda
Mouad Lasri <lasri.mouad98@gmail.com>
2019-06-25 17:00:12 +0100

    Added cards with little styling

--- src/experience.js
+++ src/experience.js
@@ -4,8 +4,29 @@ import "./experience.css";
 
 const Experience = () => {
   return (
-    <div className="experience">
-      <img src="https://via.placeholder.com/728x90.png" alt="placeholder" />
+    <div className="experience container-fluid">
+      <div className="experience-group">
+        <div className="card box">
+          <img
+            className="card-img-top"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnkHe5EgGvozEjjNo1N9zrXKwszaXLVx_hmh3o0Eon0tD02TZW"
+            alt="Card image"
+          />
+          <div class="card-body">
+            <h4 class="card-title">John Doe</h4>
+            <p class="card-text">Some example text.</p>
+            <a href="#" class="btn btn-primary">
+              See Profile
+            </a>
+          </div>
+        </div>
+        <div className=" box">
+          <p>Box 2</p>
+        </div>
+        <div className="box">
+          <p>Box 3</p>
+        </div>
+      </div>
     </div>
   );
 };
